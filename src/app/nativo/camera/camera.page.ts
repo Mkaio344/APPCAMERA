6822251a4a383d0fa73f670b48cb4934e2b9f45e
None <None>
2024-10-11 19:53:15 -0300

    camera funcionando

--- src/app/nativo/camera/camera.page.ts
+++ src/app/nativo/camera/camera.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Camera, CameraResultType} from '@capacitor/camera';
+
 @Component({
   selector: 'app-camera',
   templateUrl: './camera.page.html',
